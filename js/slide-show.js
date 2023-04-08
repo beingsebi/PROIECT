ae0255d75d2dda7d9ec8ab3842244684a951533e
Popa Sebastian <sebastianpopa208@gmail.com>
2023-04-08 15:35:09 +0300

    fix

--- js/slide-show.js
+++ js/slide-show.js
@@ -3,7 +3,6 @@ showSlides();
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    // let dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -11,11 +10,8 @@ function showSlides() {
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    // }
+
     slides[slideIndex - 1].style.display = "block";
-    // dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 6500);
 }
 
