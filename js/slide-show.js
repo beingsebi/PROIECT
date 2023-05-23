ebdc510764eabff50e858f46d766f40d9a005b0c
sebastianp2003 <sebastianp2003@yahoo.com>
2023-05-23 20:27:49 +0300

    back to top btn

--- js/slide-show.js
+++ js/slide-show.js
@@ -14,34 +14,3 @@ function showSlides() {
     slides[slideIndex - 1].style.display = "block";
     setTimeout(showSlides, 6500);
 }
-
-// const links = document.querySelectorAll("a");
-// for (let i = 0; i < links.length; i++) {
-//     links[i].addEventListener("click", function (event) {
-//         event.preventDefault();
-//         const href = this.getAttribute("href");
-//         fetch(href)
-//             .then((response) => response.text())
-//             .then((data) => {
-//                 const parser = new DOMParser();
-//                 const html = parser.parseFromString(data, "text/html");
-//                 document.body.innerHTML = html.body.innerHTML;
-//                 window.history.pushState({}, "", href.slice(0, -5));
-//             })
-//             .catch((error) => console.log(error));
-//     });
-// }
-
-// window.onload = function () {
-//     if (window.location.href.indexOf(".html") > -1) {
-//         const url = window.location.href.slice(0, -5);
-//         window.history.replaceState({}, "", url);
-//     }
-// };
-
-// window.onpopstate = function (event) {
-//     if (window.location.href.indexOf(".html") > -1) {
-//         const url = window.location.href.slice(0, -5);
-//         window.history.replaceState({}, "", url);
-//     }
-// };
